f5f42d179a1afbf5ccc54696eac6d10287c2f5b2
Qun Li Huang <qunli02@gmail.com>
2019-03-25 00:24:19 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -41,6 +41,7 @@ def until_iterator(number_of_times)
   counter = 1
   until counter == number_of_times
     phrase = "Welcome to Flatiron School's Web Development Course!"
+    puts phrase
     counter += 1
   end
 
